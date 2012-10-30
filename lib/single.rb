bfb3dcad37bf6d822fb9a38a2f037e4ba94c3232
Tristan Slominski <tristan.slominski@gmail.com>
2012-10-30 10:23:36 -0500

    method_id is already a symbol

--- lib/single.rb
+++ lib/single.rb
@@ -11,7 +11,7 @@ module Single
     end
 
     def method_missing(method_id, *args, &block)
-      instance.send "#{method_id}", *args, &block
+      instance.send method_id, *args, &block
     end
   end
 end
